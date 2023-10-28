20de77670a68e1367c71d379f4608d577670f048
NedaJavan <nedajavan747@gmail.com>
2023-10-28 20:00:53 +0330

    Add Note Class and test class of it.

---
+++ app/src/main/java/com/example/learningtestwithmitch/model/Note.kt
@@ -0,0 +1,27 @@
+package com.example.learningtestwithmitch.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Note")
+class Note {
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null
+
+    @ColumnInfo(name = "title")
+    private val title: String? = null
+
+    @ColumnInfo(name = "content")
+    private val content: String? = null
+
+    @ColumnInfo(name = "timestamp")
+    private val timestamp: String? = null
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null) return false
+        if (other !is Note) return false
+        if (title == other.title && content == other.content && id == other.id) return true
+        return false
+    }
+}
---
+++ app/src/test/java/com/example/learningtestwithmitch/model/NoteTest.kt
@@ -0,0 +1,20 @@
+package com.example.learningtestwithmitch.model
+
+import org.junit.jupiter.api.Test
+
+class NoteTest {
+
+    // compare two equal notes
+    @Test
+    fun isNotesEqual_identicalProperties() {
+        TODO("Not yet implemented")
+    }
+
+    // compare two notes with two different ids
+
+// compare two notes with different timestamps
+    //compare two notes with different titles
+    //compare two notes with different contents
+
+
+}
